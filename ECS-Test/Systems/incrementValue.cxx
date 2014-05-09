9b84afd1836b4e47a1f8bf75eeddad3b777849e0
Chris Langford <chris@hellocld.com>
2014-05-09 15:26:00 -0400

    Created IncrementValue header and definitions
    The IncrementHeader System goes through all the Entities in a Library and
    adds 1 to the value in the Value Component (if it exists in an Entity)

---
+++ ECS-Test/Systems/incrementValue.cxx
@@ -0,0 +1,25 @@
+/*
+ * Program:		incrementValue.cxx
+ * Author:		Chris Langford
+ * Date:		2014/05/09
+ * Description:		IncrementValue System class definitions
+ */
+
+#include <vector>
+
+#include "incrementValue.h"
+#include "../Components/value.h"
+
+void ECS_Test::IncrementValue::update() {
+	//iterate through all the Entities in the Library
+	//if an Entity has a component of type Value, add 1 to the value
+	std::vector<int> entities = lib.allEntityIDs();
+
+	std::vector<int>::iterator i;
+
+	for(i = entities.begin(); i<entities.end(); ++i) {
+		if(lib.hasComponent<Value>(*i)) {
+			lib.getComponent<Value>(*i).value++;
+		}
+	}
+}
---
+++ ECS-Test/Systems/incrementValue.h
@@ -0,0 +1,20 @@
+/*
+ * Program:		incrementValue.h
+ * Author:		Chris Langford
+ * Date:		2014/05/09
+ * Description:		Header for the IncrementValue class
+ */
+
+#ifndef INCREMENTVALUE_H
+#define INCREMENTVALUE_H
+
+#include "../CLD_CES/system.h"
+
+namespace ECS_Test {
+	class IncrementValue : public CLD_CES::System {
+		public:
+			void update();
+	};
+}
+
+#endif
