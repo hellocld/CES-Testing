96258dd980d833efc01123614c8f2d9eeff892e4
Chris Langford <chris@hellocld.com>
2014-07-17 13:29:47 -0400

    Removed comments
    Extensive comments moved to header. Function names also changed to fit
    Google's C++ Style Guide.

--- system.cxx
+++ system.cxx
@@ -12,52 +12,20 @@ using namespace CLD_ECS;
 
 //-----------------------------------------------------------------------------
 
-/*
- * Function:		init()
- * Description:		Used to attach a Library to the System along with any other necessary initialization operations
- * Arguments:
- * 	Library* l:	The Library the System can search through
- * Returns:		none
- */
+void System::Init() {
+}
 
-void System::init() {
-	//Like update() and shutdown(), init() is virtual and MUST be defined in inheriting Systems
+//-----------------------------------------------------------------------------
+void System::Update() {
 }
 
 //-----------------------------------------------------------------------------
 
-/*
- * Function:		update()
- * Description:		Run during the main program loop. Any operations that must be done repeatedly should go in here.
- * Arguments:		none
- * Returns:		none
- */
-
-void System::update() {
+void System::Shutdown() {
 }
 
 //-----------------------------------------------------------------------------
 
-/*
- * Function:		shutdown()
- * Description:		Similar to init(), but normally run after update().
- * Arguments:		none
- * Returns:		none
- */
-
-void System::shutdown() {
-}
-
-//-----------------------------------------------------------------------------
-
-/*
- * Function:		linkLibrary()
- * Description:		Connects System to a Library (by address)
- * Arguments:
- * 	Library& l	address of the Library
- * Returns:		none
- */
-
-void System::linkLibrary(Library& l) {
+void System::LinkLibrary(Library& l) {
 	library = &l;	
 }
