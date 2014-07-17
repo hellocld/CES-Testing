ec0233d29f8a7d3a4896589ac15cc9891b548a6c
Chris Langford <chris@hellocld.com>
2014-07-17 11:24:08 -0400

    Updated style and comments
    Fixed the include guard, and improved overall comments for doxygen doc
    generation.

--- component.h
+++ component.h
@@ -6,15 +6,32 @@
  *
  */
 
-#ifndef COMPONENT_H
-#define COMPONENT_H
+#ifndef CLD_ECS_COMPONENT_H_INCLUDED
+#define CLD_ECS_COMPONENT_H_INCLUDED
 
-#include <iostream>
-
+//! The basic Component class
+/*!
+ * Components are the building blocks of your Entities. They are essentially
+ * nothing more than data structures that are manipulated by Systems. It's
+ * usually best to keep them as simple as possible. Don't include too many
+ * different uses for any particular Component; rather, design each Component to
+ * be used for one specific purpose, so that multiple Components can be used
+ * together to create more dynamic and varied Entities.
+ *
+ * NOTE: you cannot use a raw Component object. You must create your own
+ * Components that inherit the base Component class.
+ */
 namespace CLD_ECS {
 	class Component {
 		public:
-			Component() {std::cerr<<"New Component created\n";};
+			//! The Component constructor
+			Component() {};
+			//! The Component destructor
+			/*!
+			 * A virtual destructor is used so that accessing
+			 * Components in the Library acquires the correct
+			 * Components, rather than the base class
+			 */
 			virtual ~Component() = default;
 	};
 }
