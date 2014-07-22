5841a2bb9d794ceb45f3344def5f292121f2ea6c
Chris Langford <chris@hellocld.com>
2014-07-22 15:59:43 -0400

    Moved all definitions to header
    Decided to make this a header-only library, to simplify
    compilation/linking

--- library.h
+++ library.h
@@ -32,7 +32,16 @@ namespace CLD_ECS {
 			 * Cleans up any remaining Entities or Components still 
 			 * stored, removing them from the free store.
 			 */
-			~Library();
+			~Library(){
+				//we need to loop through everything in entities to remove EVERYTHING allocated in memory, so we'll need a couple iterators
+				for(auto id = entities.begin(); id != entities.end(); ++id) {
+					//now loop through each component
+					for(auto c = id->second.begin(); c != id->second.end(); ++c) {
+						//delete away!
+						delete c->second;
+					}
+				}
+			}
 			//! Creates a new Entity
 			/*!
 			 * Adds a new Entity to entities and returns the Entity's 
@@ -40,7 +49,29 @@ namespace CLD_ECS {
 			 * \return The new Entity's ID
 			 * \sa DestroyEntity()
 			 */
-			int CreateEntity();
+			int CreateEntity(){
+				//temporary storage for the Entity ID
+				int e;
+
+				if(entity_buffer.size() == 0) {
+					//double-check to make sure entity_index isn't already in use as an ID
+					while(entities.count(entity_index) != 0) {
+						//if it is, just increase by one and check again
+						entity_index++;
+					}
+					e = entity_index;
+					entity_index++;
+				} else {
+					//since there's something in the buffer, remove the last entry from 
+					//entity_buffer and return it
+					e = entity_buffer.back();
+					entity_buffer.pop_back();
+				}
+				//create a new entry in entities
+				entities[e][&typeid(BasicComp)] = new BasicComp();
+				return e;
+			}
+
 			//! Destroys an Entity
 			/*!
 			 * Removes an Entity from entities. If the
@@ -52,8 +83,25 @@ namespace CLD_ECS {
 			 *         successfully
 			 * \sa CreateEntity(), GetEntityIDs()
 			 */
-			bool DestroyEntity(int e);
-			
+			bool DestroyEntity(int e){
+				//look in entities to see if there's a key that matches e
+				if(entities.count(e) != 0) {
+					//loop through all it's components and delete them
+					for(auto c = entities[e].begin(); c != entities[e].end(); ++c) {
+						delete c->second;
+					}
+					//destroy the key and associated value
+					entities.erase(e);
+					//since e is now an unused ID, push it back into entity_buffer for reuse
+					entity_buffer.push_back(e);
+
+					return true;
+				} else {
+					//e was not a valid ID
+					return false;
+				}
+			}
+
 			//! Adds a new Component to Entity e
 			/*!
 			 * AddComponent creates a new Component on the free
@@ -66,7 +114,25 @@ namespace CLD_ECS {
 			 * \return True if successful
 			 * \sa CreateEntity(), GetEntityIDs()
 			 */
-			bool AddComponent(int e, Component* c);
+			bool AddComponent(int e, Component* c){
+				//check if e is a valid Entity ID
+				if(entities.count(e) != 0) {
+					//make sure the Entity doesn't already contain a Component of c's type
+					if(entities[e].count(&typeid(*c)) != 0) {
+						//ERROR: Entity e has a Component of c's type already
+						return false;
+					} else {
+						//add the Component with it's typeid as the key
+						entities[e][&typeid(*c)] = c;
+						return true;
+					}
+				} else {
+					//ERROR: e is not a valid Entity ID
+					return false;
+				}
+			}
+
+
 			//! Removes a Component from Entity e
 			/*!
 			 * Removes a Component of type T from Entity with ID e.
@@ -76,7 +142,24 @@ namespace CLD_ECS {
 			 * \return True if successful
 			 * \sa RemoveComponent(), GetComponent(), HasComponent()
 			 */
-			template <typename T> bool RemoveComponent(int e);
+			template <typename T> bool RemoveComponent(int e){
+				//check to see if Entity e exists
+				if(entities.count(e) != 0) {
+					//check for the Component
+					if(entities[e].count(&typeid(T)) != 0) {
+						//it exists, so remove the Component
+						delete *entities[e][&typeid(T)];
+						entities[e].erase(&typeid(T));
+						return true;
+					} else {
+						//the Component was not found
+						return false;
+					}
+				} else {
+					//ERROR: e is not a valid Entity ID
+					return false;
+				}
+			}
 
 			//! Gets a Component from Entity e
 			/*!
@@ -88,8 +171,21 @@ namespace CLD_ECS {
 			 *         Component was not found
 			 * \sa AddComponent(), GetComponent(), HasComponent()
 			 */
-			template <typename T> T* GetComponent(int e);
-			
+			template <typename T> T* GetComponent(int e){
+				if(entities.count(e) != 0) {
+					if(entities[e].count(&typeid(T)) != 0) {
+						//return a pointer to the Component
+						return dynamic_cast<T*>(entities[e][&typeid(T)]);
+					} else {
+						//ERROR: no such Component exists in Entity e
+						return nullptr;
+					}
+				} else {
+					//ERROR: no such Entity exists
+					return nullptr;
+				}
+			}
+
 			//! Checks if Entity e has a Component of type T
 			/*!
 			 * \param T the type of Component to search for
@@ -97,7 +193,16 @@ namespace CLD_ECS {
 			 * \return True if Entity e has a Component of type T
 			 * \sa AddComponent(), RemoveComponent(), GetComponent
 			 */
-			template <typename T> bool HasComponent(int e);
+			template <typename T> bool HasComponent(int e){
+				if(entities.count(e) != 0) {
+					if(entities[e].count(&typeid(T)) != 0) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+				return false;
+			}
 
 			//! Returns a vector of all active Entity IDs
 			/*!
@@ -105,7 +210,17 @@ namespace CLD_ECS {
 			 *         ID
 			 * \sa CreateEntity(), DestroyEntity()
 			 */
-			std::vector<int> GetEntityIDs();
+			std::vector<int> GetEntityIDs(){
+				//an empty vector to fill with Entity IDs
+				std::vector<int> id;
+
+				//iterate through the keys in entities, pushing each one into id
+				for(auto i = entities.begin(); i != entities.end(); ++i) {
+					id.push_back(i->first);
+				}
+
+				return id;
+			}
 
 		private:
 			//! An unordered map that stores all entities
@@ -132,8 +247,6 @@ namespace CLD_ECS {
 			std::vector<int> entity_buffer;
 	};
 
-	//the template function definitions
-	#include "library.txx"
 
 	//creating an entity requires a single basic Component, so here's one that every Entity can use
 	class BasicComp : public Component {
