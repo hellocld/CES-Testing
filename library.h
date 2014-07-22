f4418886871dec1d841ee667a49d99759a327082
Chris Langford <chris@hellocld.com>
2014-07-22 16:10:23 -0400

    Added license
    Forgot to copy the license boilerplate to Library

--- library.h
+++ library.h
@@ -1,8 +1,27 @@
 /*
- * Program:		library.h
- * Author:		Chris Langford
- * Date:		2014/05/05
- * Description:		Header file for the Library class in the CLD_ECS framework 
+ * library.h -- header for the Library class, part of CLD-ECS
+ * version 1.0, July 22nd, 2014
+ *
+ * Copyright (c) 2014 cld
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source
+ *    distribution.
  */
 
 
